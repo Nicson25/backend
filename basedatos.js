86dd202adc3d8cf8b4cfaa486eacecb60acc3ac1
Nicson25 <nixonandres_96@hotmail.com>
2026-04-27 17:53:47 -0500

    Update basedatos.js

--- basedatos.js
+++ basedatos.js
@@ -2,7 +2,7 @@ const mysql =require("mysql2");
 
 const basedatos = mysql.createConnection({
     host: process.env.DB_HOST,
-    user: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,  // contraseña cuenta
     database: process.env.DB_NAME
 });
